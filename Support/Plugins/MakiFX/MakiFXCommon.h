3ce49ab1f576fa19642b9324c43e7c2759f7d6f9
THMF2K10 (Backup and Main) <tantamoraru49@gmail.com>
2026-02-28 17:52:08 +0200

    Create MakiFXCommon.h
    Signed-off-by: THMF2K10 (Backup and Main) <tantamoraru49@gmail.com>

---
+++ Support/Plugins/MakiFX/MakiFXCommon.h
@@ -0,0 +1,63 @@
+#ifndef MAKIFXCOMMON_H
+#define MAKIFXCOMMON_H
+
+#ifdef _WIN64
+#include <Windows.h>
+#else
+#include <pthread.h>
+#endif
+#include <map>
+#include <math.h>
+#include <stdio.h>
+
+class Locker
+{
+public:
+    Locker()
+    {
+#ifdef _WIN64
+        InitializeCriticalSection(&mutex);
+#else
+        pthread_mutex_init(&mutex, NULL);
+#endif
+    }
+
+    ~Locker()
+    {
+#ifdef _WIN64
+        DeleteCriticalSection(&mutex);
+#else
+        pthread_mutex_destroy(&mutex);
+#endif
+    }
+
+    void Lock()
+    {
+#ifdef _WIN64
+        EnterCriticalSection(&mutex);
+#else
+        pthread_mutex_lock(&mutex);
+#endif
+    }
+
+    void Unlock()
+    {
+#ifdef _WIN64
+        LeaveCriticalSection(&mutex);
+#else
+        pthread_mutex_unlock(&mutex);
+#endif
+    }
+
+private:
+#ifdef _WIN64
+    CRITICAL_SECTION mutex;
+#else
+    pthread_mutex_t mutex;
+#endif
+};
+
+extern float buffers[64][16][25];
+extern int transformIndex;
+extern double frameRate;
+#endif
